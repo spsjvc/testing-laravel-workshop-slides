a9965772fb0d7b662d5c56afdfb86ba5113fcb29
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-06-12 18:31:26 +0200

    Finish slides for the first workshop

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function({ children }) {
+  return (
+    <p
+      style={{
+        fontSize: '24px',
+        position: 'absolute',
+        marginTop: '-72px'
+      }}
+    >
+      {children}
+    </p>
+  )
+}
